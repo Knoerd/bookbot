087bc323ccd38b3bb9b7ce82809ad9ed655a9aca
Sjoerd te Vruchte <sjoerdtevruchte@hotmail.com>
2025-11-09 22:20:26 +0100

    bookbot CH3 L3

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import count_unique_characters, count_words_in_string, sort_dictionary
 
 
@@ -8,14 +10,18 @@ def get_book_text(file):
 
 
 def main():
-    book_file = "./books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_file = sys.argv[1]
     book_text = get_book_text(book_file)
     book_words = count_words_in_string(book_text)
     character_count = count_unique_characters(book_text)
     sorted_characters = sort_dictionary(character_count)
 
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {book_file}...")
     print("----------- Word Count ----------")
     print(f"Found {book_words} total words")
     print("--------- Character Count -------")
